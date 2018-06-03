8cf52aa2a54ab00550da724adc1250a19c57cc24
Dmitry <dmitry26-dev@yahoo.com>
2018-06-03 16:32:46 -0400

    Migrated to .NET Core 2.1/ASP.NET Core 2.1

--- src/Samples/SignalRWebConsole/Startup.cs
+++ src/Samples/SignalRWebConsole/Startup.cs
@@ -4,6 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Serilog;
@@ -23,8 +25,15 @@ namespace SignalRWebConsole
 		// This method gets called by the runtime. Use this method to add services to the container.
 		public void ConfigureServices(IServiceCollection services)
 		{
+			services.Configure<CookiePolicyOptions>(options =>
+			{
+				// This lambda determines whether user consent for non-essential cookies is needed for a given request.
+				options.CheckConsentNeeded = context => true;
+				options.MinimumSameSitePolicy = SameSiteMode.None;
+			});
+
 			services.AddSignalR();
-			services.AddMvc();
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -32,7 +41,6 @@ namespace SignalRWebConsole
 		{
 			if (env.IsDevelopment())
 			{
-				app.UseBrowserLink();
 				app.UseDeveloperExceptionPage();
 			}
 			else
@@ -41,7 +49,7 @@ namespace SignalRWebConsole
 			}
 
 			app.UseStaticFiles();
-
+			app.UseCookiePolicy();
 			app.UseFileServer();
 
 			app.UseSignalR(routes =>
