6687f2bd166d8326fb3bbe43d186fd839eefd871
Dmitry <dmitry26-dev@yahoo.com>
2019-09-03 22:15:50 -0400

    Upgraded to .NET Core v2.2.6, SignalR v1.1.0, etc. Updated npm packages to fix security vulnerability.

--- src/Samples/SignalRWebConsole/Startup.cs
+++ src/Samples/SignalRWebConsole/Startup.cs
@@ -33,7 +33,7 @@ namespace SignalRWebConsole
 			});
 
 			services.AddSignalR();
-			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+			services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
 		}
 
 		// This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
